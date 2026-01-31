c966722676d2f7613d1365a72bd893b5b6a779e1
rtorrent <rtorrent@student.codam.nl>
2026-01-31 02:38:15 +0100

    Numeric responses return the client's nickname, not the full username

--- src/commands/invite.cpp
+++ src/commands/invite.cpp
@@ -3,15 +3,17 @@
 void EventLoop::invite(Client *client, const std::deque<std::string>& p)
 {
 	User *user = client->getUser();
+	const std::string nick = user->getNickname();
+
 	if (!user->isRegistered()) {
 		client->response(server.getName(), ERR_NOTREGISTERED,
-							client->getName() + ' ' + 
+							nick + ' ' + 
 							ERR_NOTREGISTERED_MESSAGE);
 		return ;
 	}
 	if (p.size() < 2) {
 		client->response(server.getName(), ERR_NEEDMOREPARAMS,
-							client->getName() + 
+							nick + 
 							 " INVITE " + 
 							 ERR_NEEDMOREPARAMS_MESSAGE);		
 		return ;
@@ -27,28 +29,28 @@ void EventLoop::invite(Client *client, const std::deque<std::string>& p)
 	}
 	if (!channel) {
 		client->response(server.getName(), ERR_NOSUCHCHANNEL,
-							client->getName() + ' ' + 
+							nick + ' ' + 
 							channelName + ' ' + 
 							ERR_NOSUCHCHANNEL_MESSAGE);
 		return ;		
 	}
 	if (!channel->hasClient(client)) {
 		client->response(server.getName(), ERR_NOTONCHANNEL,
-							client->getName() + ' ' + 
+							nick + ' ' + 
 							channelName + ' ' + 
 							ERR_NOTONCHANNEL_MESSAGE);
 		return ;		
 	}
 	if (!channel->isOperator(client)) {
 		client->response(server.getName(), ERR_CHANOPRIVSNEEDED,
-							client->getName() + ' ' + 
+							nick + ' ' + 
 							channelName + ' ' + 
 							ERR_CHANOPRIVSNEEDED_MESSAGE);
 		return ;
 	}
 	if (channel->hasClient(invitee)) {
 		client->response(server.getName(), ERR_USERONCHANNEL,
-							client->getName() + ' ' + 
+							nick + ' ' + 
 							channelName + ' ' + 
 							invitee->getUser()->getNickname() + ' ' + 
 							ERR_USERONCHANNEL_MESSAGE);
@@ -57,7 +59,7 @@ void EventLoop::invite(Client *client, const std::deque<std::string>& p)
 
 	/* Sending confirmation to the requester */
 	client->response(server.getName(), RPL_INVITING,
-					client->getName() + ' ' +
+					nick + ' ' +
 					invitee->getUser()->getNickname() + ' ' +
 					channelName);
 
